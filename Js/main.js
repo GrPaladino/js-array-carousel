b5f5979244dd2441c47b1aa6985c6ca76020a35e
GrPaladino <paladino.graziano@gmail.com>
2024-01-15 15:42:45 +0100

    Feat: change img on arrow-up press

--- Js/main.js
+++ Js/main.js
@@ -4,18 +4,43 @@
 
 // # ELEMENTO HTML
 const slidesContainerEl = document.getElementById("slides-container");
+const arrowUp = document.getElementById("arrow-up");
+const arrowDown = document.getElementById("arrow-down");
 
 // # ARRAY SLIDES
 const slides = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
+
+// Definisco l'immagine da mostrare al caricamento
+let currentImg = 0;
 
 // # CICLO FOR ON LOAD
 let slideHtml = "";
 for (let i = 0; i < slides.length; i++) {
   const slide = slides[i];
 
-  let visibleClass = i == 0 ? "visible" : "";
+  let visibleClass = i == currentImg ? "visible" : "";
 
   slideHtml += `<img src="./img/${slide}" class="slide ${visibleClass}" alt="slide-${i}" />`;
-  console.log(slide);
 }
 slidesContainerEl.innerHTML = slideHtml;
+
+// # CAMBIO IMMAGINE AL CLICK ARRROW-UP
+arrowUp.addEventListener("click", function () {
+  // eliminazione classe precedente
+  const currentSlide = document.querySelector(".slide.visible");
+  currentSlide.classList.remove("visible");
+
+  //   incremento delle img
+  const allSlides = document.getElementsByClassName("slide");
+
+  if (currentImg >= allSlides.length - 1) {
+    currentImg = 0;
+  } else {
+    currentImg++;
+  }
+
+  //   aggiunta classe alla prossima img
+
+  const newImg = allSlides[currentImg];
+  newImg.classList.add("visible");
+});
